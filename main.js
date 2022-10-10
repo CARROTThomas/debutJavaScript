ad6203d7f961736b1bf7144cea9c590a218e3091
CARROTThomas <thomas.carrot38240@gmail.com>
2022-10-10 13:00:35 +0200

    avancer des methods JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("coucou")
+let word = "bonjour"
+
+//let newWord = word.replace(/our/, 'ur');
+
+//let newWord = word.replace(/o/,'')
+//let bonjur = word + (word[4]='R');
+let tableau = word.split("")
+
+tableau.splice(4, 1)
+
+console.log(tableau)
+let wordFinal ="";
+
+for (let i = 0; i < tableau.length; i++) {
+    wordFinal = wordFinal + tableau[i]
+}
+console.log(wordFinal);
+
+
+
+
+
+
+const monParagraphe = document.querySelector(".rouge")
+
+function changerCouleur (){
+    monParagraphe.classList.toggle("bleu")
+}
+setInterval(changerCouleur, 1000)
+//setTimeout(changerCouleur,1000)
+
+
+let allBonjour = ['Bonjour', 'coucou', 'hello', 'salut', 'hola'];
+
+function changementDeMot(mot){
+    monParagraphe.textContent = mot;
+}
+
+
+for (let i = 0; i < allBonjour.length; i++) {
+    let mot = allBonjour[i];
+    changementDeMot(mot)
+    console.log(mot);
+    clearTimeout(1000);
+}
